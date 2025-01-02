6130992e127e94cf36cfaca5431f2b2019fb8a32
Sophie <sophiaasophieee@gmail.com>
2025-01-02 01:54:38 -0500

    test

--- script.js
+++ script.js
@@ -2,31 +2,24 @@ $(document).ready(function() {
   var snowflakeCount = 100;
   var snowflakeImages = [
     'https://i.imgur.com/RDSMKvt.png', // Image 1
-    'https://i.imgur.com/another-image.gif', // GIF 1
-    'https://i.imgur.com/yet-another-image.gif' // GIF 2
+    'https://media.tenor.com/aW_E-EO9smoAAAAM/halloween-happy.gif', // GIF 1
   ];
 
   for (var i = 0; i < snowflakeCount; i++) {
-    var isGifSnowflake = Math.random() < 0.5; // 50% chance to be a GIF snowflake
-    var size = Math.random() * 30 + 20; // Size between 20px and 50px
+    var isImageSnowflake = Math.random() < 0.5; // 50% chance to be an image snowflake
+    var size = Math.random() * 25 + 5; // Size between 20px and 50px
     var snowflake = $('<div class="snowflake"></div>').css({
       left: Math.random() * 100 + 'vw',
-      top: '-' + (Math.random() * 20 + 10) + 'px', // Spawn slightly above the screen
       width: size + 'px',
       height: size + 'px',
+      animationDuration: Math.random() * 3 + 2 + 's',
+      animationDelay: Math.random() * 2 + 's',
     });
 
-    if (isGifSnowflake) {
-      var randomGif = snowflakeImages[Math.floor(Math.random() * snowflakeImages.length)];
+    if (isImageSnowflake) {
+      var randomImage = snowflakeImages[Math.floor(Math.random() * snowflakeImages.length)];
       snowflake.addClass('snowflake-image').css({
-        backgroundImage: 'url(' + randomGif + ')',
-        animationDuration: Math.random() * 5 + 7 + 's', // Slower fall duration for GIFs
-      });
-    } else {
-      var randomImage = 'https://i.imgur.com/RDSMKvt.png'; // Default image for other snowflakes
-      snowflake.addClass('snowflake-image').css({
-        backgroundImage: 'url(' + randomImage + ')',
-        animationDuration: Math.random() * 3 + 2 + 's', // Faster fall duration for images
+        backgroundImage: 'url(' + randomImage + ')'
       });
     }
 
