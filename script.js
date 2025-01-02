d3f7c463e303b9231ae0289f83062c6957c4f113
Sophie <sophiaasophieee@gmail.com>
2025-01-02 01:32:10 -0500

    aaa

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ $(document).ready(function() {
 
   for (var i = 0; i < snowflakeCount; i++) {
     var isImageSnowflake = Math.random() < 0.5; // 50% chance to be an image snowflake
+    var scale = Math.random() * 0.5 + 0.5; // Scale between 0.5 and 1.0
     var snowflake = $('<div class="snowflake"></div>').css({
       left: Math.random() * 100 + 'vw',
-      width: Math.random() * 5 + 5 + 'px',
-      height: Math.random() * 5 + 5 + 'px',
+      width: (Math.random() * 5 + 5) * scale + 'px',
+      height: (Math.random() * 5 + 5) * scale + 'px',
       animationDuration: Math.random() * 3 + 2 + 's',
       animationDelay: Math.random() * 2 + 's',
     });
